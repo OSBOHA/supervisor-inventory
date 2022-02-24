62b7f9ca0c68e1c944c18f25ed0640b488b18bbf
Sharif Omer <Abdonline995@gmail.com>
2022-02-24 11:51:40 -0800

    include header and footer for login and register pages

---
+++ resources/views/auth/includes/footer.blade.php
@@ -0,0 +1,17 @@
+
+ <!--===============================================================================================-->
+     <script src="{{asset('loginpage/vendor/jquery/jquery-3.2.1.min.js')}}"></script> 
+<!--===============================================================================================-->
+	<script src="{{asset('loginpage/vendor/animsition/js/animsition.min.js')}}"></script>
+<!--===============================================================================================-->
+	<script src="{{asset('loginpage/vendor/bootstrap/js/popper.js')}}"></script>
+	<script src="{{asset('loginpage/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+<!--===============================================================================================-->
+	<script src="{{asset('loginpage/vendor/select2/select2.min.js')}}"></script>
+<!--===============================================================================================-->
+	<script src="{{asset('loginpage/vendor/daterangepicker/moment.min.js')}}"></script>
+	<script src="{{asset('loginpage/vendor/daterangepicker/daterangepicker.js')}}"></script>
+<!--===============================================================================================-->
+	<script src="{{asset('loginpage/vendor/countdowntime/countdowntime.js')}}"></script>
+<!--===============================================================================================-->
+	<script src="{{asset('loginpage/js/main.js')}}"></script>
