751fb9994318fd1eed4e6a4205a18d697a8da1b3
Bushra-Khaled <boushra.nnm@gmail.com>
2022-02-28 21:25:03 +0200

    leader duty

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/leaderduty', [App\Http\Controllers\LeaderDutyController::class, 'index']);
