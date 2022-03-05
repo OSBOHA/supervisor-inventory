9b1e0b0ee063b76c30c55b1aad517706d8257898
hafsabd0208 <hafsa02bedoui@gmail.com>
2022-03-05 21:37:11 +0100

    update supervisor-page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,7 +45,72 @@
                             </a>
                         </li>
                     </ul>
-                </div>
+                    <ul class="menu">
+            <li
+             class="sidebar-item  has-sub">
+               <a href="#" class='sidebar-link'>
+               <h5><i class="bi bi-person-circle"></i></h5>
+                  <span> <h5> Super Test </h5></span>
+                  <ul class="submenu ">
+                    <div class="submenu-item" >
+                        <a href="#" class="submenu-item" >
+                        <i class="bi bi-envelope-fill"></i> 
+                        <span>استفسارات الموجه</span>
+
+                    </div>
+                   
+                    
+                  </ul>  
+                    
+            </li>
+                  
+            
+            <li
+            class="sidebar-item  ">
+                <a href="#" class='sidebar-link'>
+                <i class="bi bi-person-lines-fill"></i>
+                    <span>بياناتك الشخصية</span>
+                </a>
+                
+            </li>
+            
+            <li
+                class="sidebar-item ">
+                <a href="#" class='sidebar-link'>
+                    <i class="bi bi-pencil-square"></i>
+                    <span>الجرد لأسبوعي</span>
+                </a>
+            </li>
+            
+            <li
+                class="sidebar-item  ">
+                <a href="#" class='sidebar-link'>
+                    <i class="bi bi-receipt-cutoff"></i>
+                    <span>عرض نتيجة الجرد</span>
+                </a>
+                
+            </li>
+            
+            <li
+                class="sidebar-item ">
+                <a href="form-layout.html" class='sidebar-link'>
+                    <i class="bi bi-exclamation-circle-fill"></i>
+                    <span>صندوق اعتراضاتك</span>
+                </a>
+            </li>
+            
+            <li class="sidebar-item  ">
+                <a href="form-layout.html" class='sidebar-link'>
+                    <i class="bi bi-reception-4"></i>
+                    <span>ترتيبك الرقابي</span>
+                </a>
+                
+            </li>
+            
+            
+        </ul>
+
+        </div>
                 <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
             </div>
         </div>
