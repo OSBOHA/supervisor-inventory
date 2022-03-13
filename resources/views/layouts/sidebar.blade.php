634e759fbdb0f232522b506a867e9826da163664
saraIsmailSE <saraismailse@gmail.com>
2022-03-13 15:10:51 +0300

    edit temp.

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,89 @@
+<div id="app">
+    <div id="sidebar" class="active">
+        <div class="sidebar-wrapper active">
+            <div class="sidebar-header">
+                <div class="d-flex justify-content-between">
+                    <div class="logo">
+                        <a href="#"><img src="assets/images/logo/logo.png" alt="Logo" srcset=""></a>
+                    </div>
+                    <div class="toggler">
+                        <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="sidebar-menu">
+                <ul class="menu">
+                    <li class="sidebar-item active ">
+                        <a href="#" class='sidebar-link'>
+                            <i class="bi bi-grid-fill"></i>
+                            <span>Leader Duty</span>
+                        </a>
+                    </li>
+                </ul>
+                <ul class="menu">
+                    <li class="sidebar-item  has-sub">
+                        <a href="#" class='sidebar-link'>
+                            <h5><i class="bi bi-person-circle"></i></h5>
+                            <span>
+                                <h5> Super Test </h5>
+                            </span>
+                            <ul class="submenu ">
+                                <div class="submenu-item">
+                                    <a href="#" class="submenu-item">
+                                        <i class="bi bi-envelope-fill"></i>
+                                        <span>استفسارات الموجه</span>
+
+                                </div>
+
+
+                            </ul>
+
+                    </li>
+
+
+                    <li class="sidebar-item  ">
+                        <a href="#" class='sidebar-link'>
+                            <i class="bi bi-person-lines-fill"></i>
+                            <span>بياناتك الشخصية</span>
+                        </a>
+
+                    </li>
+
+                    <li class="sidebar-item ">
+                        <a href="#" class='sidebar-link'>
+                            <i class="bi bi-pencil-square"></i>
+                            <span>الجرد لأسبوعي</span>
+                        </a>
+                    </li>
+
+                    <li class="sidebar-item  ">
+                        <a href="#" class='sidebar-link'>
+                            <i class="bi bi-receipt-cutoff"></i>
+                            <span>عرض نتيجة الجرد</span>
+                        </a>
+
+                    </li>
+
+                    <li class="sidebar-item ">
+                        <a href="form-layout.html" class='sidebar-link'>
+                            <i class="bi bi-exclamation-circle-fill"></i>
+                            <span>صندوق اعتراضاتك</span>
+                        </a>
+                    </li>
+
+                    <li class="sidebar-item  ">
+                        <a href="form-layout.html" class='sidebar-link'>
+                            <i class="bi bi-reception-4"></i>
+                            <span>ترتيبك الرقابي</span>
+                        </a>
+
+                    </li>
+
+
+                </ul>
+
+            </div>
+            <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+        </div>
+    </div>
+</div>
