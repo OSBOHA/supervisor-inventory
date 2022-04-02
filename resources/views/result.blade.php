74f758a87659f887f26789b2f42d76b46d009247
Enas11 <enaskasem1@outlook.sa>
2022-04-02 18:20:00 +0300

    modify div

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -9,7 +9,7 @@
     </div>
     <div class="page-content">
         <section class="row">
-            <div class="col-12 col-lg-9">
+            <div class="col-12 col-lg-12">
             <div class="row">
                        
                             <div class="card">
