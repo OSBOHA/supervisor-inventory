cb3fbcadff612417b9e5046624879181d03505e6
saraIsmailSE <44667136+saraIsmailSE@users.noreply.github.com>
2022-04-10 15:40:26 +0300

    Revert "New leader duty"

--- app/Traits/MediaTraits.php
+++ app/Traits/MediaTraits.php
@@ -5,10 +5,10 @@ use Illuminate\Support\Facades\File;
 Trait MediaTraits{
 
     function createMedia($media){
-        $imageName = time().'.'. $media->extension();
-        $media->move(public_path('assets/images'), $imageName);
+        $imageName = time().'.'. $media->extension();  
+        $media->move(public_path('assets/images'), $imageName);    
         // return media name
-        return $imageName;
+        return $imageName;  
     }
 
 
