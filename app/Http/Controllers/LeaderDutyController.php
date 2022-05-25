fff818392bbe8f6d076537e231d44061349d59aa
Bushra-Khaled <boushra.nnm@gmail.com>
2022-05-25 05:39:42 +0200

    new update

--- app/Http/Controllers/LeaderDutyController.php
+++ app/Http/Controllers/LeaderDutyController.php
@@ -50,6 +50,8 @@ class LeaderDutyController extends Controller
 
         ]);
 
+
+
         if ($request->hasFile('leader_message_1')){
             $leader_message_1 = $this->createMedia($request->file('leader_message_1'));
         } else {$leader_message_1= "null";}
@@ -68,18 +70,53 @@ class LeaderDutyController extends Controller
 
 
 
-         $follow_up_array = serialize(array(["$request->follow_up_post",
-         "$request->follow_up_standard_1", "$request->follow_up_standard_2", "$request->follow_up_standard_3", "$request->follow_up_standard_4", "$request->follow_up_standard_5", "$request->follow_up_standard_6", "$request->follow_up_standard_7", "$request->follow_up_standard_8", "$request->follow_up_standard_9", "$request->follow_up_standard_10"]));
+         $follow_up_array = serialize(array([
+            "follow_up_post" =>"$request->follow_up_post",
+            "follow_up_standard_1" => "$request->follow_up_standard_1",
+            "follow_up_standard_2" => "$request->follow_up_standard_2",
+            "follow_up_standard_3" => "$request->follow_up_standard_3",
+            "follow_up_standard_4" => "$request->follow_up_standard_4",
+            "follow_up_standard_5" => "$request->follow_up_standard_5",
+            "follow_up_standard_6" => "$request->follow_up_standard_6",
+            "follow_up_standard_7" => "$request->follow_up_standard_7",
+            "follow_up_standard_8" => "$request->follow_up_standard_8",
+            "follow_up_standard_9" => "$request->follow_up_standard_9",
+            "follow_up_standard_10" => "$request->follow_up_standard_10"
+        ]));
 
-         $support_array = serialize(array(["$request->support_post", "$request->support_standard_1", "$request->support_standard_2", "$request->support_standard_3", "$request->support_standard_4", "$request->support_standard_5", "$request->support_standard_6", "$request->support_standard_7"]));
+         $support_array = serialize(array([
+            "support_post" =>"$request->support_post",
+            "support_standard_1" => "$request->support_standard_1",
+            "support_standard_2" =>"$request->support_standard_2",
+            "support_standard_3" =>"$request->support_standard_3",
+            "support_standard_4" =>"$request->support_standard_4",
+            "support_standard_5" =>"$request->support_standard_5",
+            "support_standard_6" =>"$request->support_standard_6",
+            "support_standard_7" =>"$request->support_standard_7"]));
 
-         $news_array = serialize(array([" leader, $request->news_leader", "discussion, $request->news_discussion", "writing, $request->news_writing" ]));
+         $news_array = serialize(array([
+            "leader" => "$request->news_leader",
+            "discussion" => "$request->news_discussion",
+            "writing"=> "$request->news_writing" ]));
 
-         $elementary_mark_array = serialize(array(["$request->elementary_mark", "$request->elementary_standard_1", "$request->elementary_standard_2", "$request->elementary_standard_3", "$request->elementary_standard_4", "$request->elementary_standard_5"]));
+         $elementary_mark_array = serialize(array([
+            "elementary_mark"=> "$request->elementary_mark",
+            "elementary_standard_1" => "$request->elementary_standard_1",
+            "elementary_standard_2" => "$request->elementary_standard_2",
+            "elementary_standard_3" => "$request->elementary_standard_3",
+            "elementary_standard_4" => "$request->elementary_standard_4",
+            "elementary_standard_5" => "$request->elementary_standard_5"]));
 
-         $audit_final_mark_array = serialize(array([ $request->audit_final_mark, $leader_message_1, $leader_message_2, $leader_message_3, $leader_reply_message ]));
+         $audit_final_mark_array = serialize(array([
+            "audit_final_mark" => $request->audit_final_mark,
+            "leader_message_1" => $leader_message_1,
+            "leader_message_2" => $leader_message_2,
+            "leader_message_3" => $leader_message_3,
+            "leader_reply_message" => $leader_reply_message ]));
 
-         $withdrawn_ambassadors_array = serialize(array($request->withdrawn_ambassadors, $request->num_defective));
+         $withdrawn_ambassadors_array = serialize(array(
+            "withdrawn_ambassadors" => "$request->withdrawn_ambassadors",
+            "defective_num" => "$request->num_defective"));
 
           $leaderduty= LeaderDuty::create([
             'leader_id'=> $request->leader_id ,
@@ -94,10 +131,11 @@ class LeaderDutyController extends Controller
             'final_mark' =>$request->final_mark,
             'audit_final_mark' =>$audit_final_mark_array,
             'withdrawn_ambassadors' =>$withdrawn_ambassadors_array
-
         ]);
 
-        return redirect()->route('index')->with('success', 'Your Entry Saved');
+              // dd($leaderduty);
+
+        return redirect()->route('index')->with('message', 'Your Entry Saved');
     }
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,13 +15,13 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
-
     <link rel="stylesheet" href="{{asset('assets/vendors/perfect-scrollbar/perfect-scrollbar.css')}}">
     <link rel="stylesheet" href="{{asset('assets/vendors/bootstrap-icons/bootstrap-icons.css')}}">
     <link rel="stylesheet" href="{{asset('assets/css/app.rtl.css')}}">
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.svg')}}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('assets/vendors/toastify/toastify.css')}}">
     <link rel="stylesheet" href="assets/vendors/choices.js/choices.min.css" />
+
 </head>
 
 <body>
