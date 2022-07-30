27b385ca1026b8ce09ca3a660cd85c2e9c564bad
saraIsmailSE <saraismailse@gmail.com>
2022-07-31 00:05:00 +0300

    ..

--- database/migrations/2022_02_20_155414_create_leadres_table.php
+++ database/migrations/2022_02_20_155414_create_leadres_table.php
@@ -13,7 +13,7 @@ class CreateLeadresTable extends Migration
      */
     public function up()
     {
-        Schema::create('leadres', function (Blueprint $table) {
+        Schema::create('leaders', function (Blueprint $table) {
             $table->id();  
             $table->string('name');
             $table->integer('advisor_id');
